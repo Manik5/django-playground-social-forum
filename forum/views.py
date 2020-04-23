543d70b8335b646609b94621f1d21e897a1fe0b1
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-23 15:01:43 +0200

    added admin and user privilege

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
 # Create your views here.
+from .mixins import StaffMixing
+from .models import Section
+class CreateSection(StaffMixing, CreateView):
+  model = Section
+  fields = "__all__"
+  template_name = "forum/create_section.html"
+  success_url = "/"
