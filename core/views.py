f5b4f6e7c1173106cd9b8da1b30331086af9d8f5
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-24 15:06:16 +0200

    added style in user list

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from forum.models import Section
 from django.views.generic.list import ListView
@@ -7,17 +8,18 @@ from django.views.generic.list import ListView
 # def homepage(request):
 #   return render(request, 'core/homepage.html')
 
-class HomeView(ListView):
-  queryset = Section.objects.all()
-  template_name = 'core/homepage.html'
-  context_object_name = "list_section"
 
-class UserList(ListView):
-  model = User
-  template_name = 'core/users.html'
+class HomeView(ListView):
+    queryset = Section.objects.all()
+    template_name = 'core/homepage.html'
+    context_object_name = "list_section"
+
+
+class UserList(LoginRequiredMixin, ListView):
+    model = User
+    template_name = 'core/users.html'
 
 def userProfileView(request, username):
-  user = get_object_or_404(User, username=username)
-  context = {"user": user}
-  return render(request, 'core/user_profile.html', context)
-
+    user = get_object_or_404(User, username=username)
+    context = {"user": user}
+    return render(request, 'core/user_profile.html', context)
