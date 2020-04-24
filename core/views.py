a875cd93c467d297ce94c4c0ee63ebdc93945eee
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-24 19:46:03 +0200

    implemented search function

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from forum.models import Discussion, Section
+from django.shortcuts import render, redirect, get_object_or_404
+from forum.models import Discussion, Post, Section
 from django.views.generic.list import ListView
 # Create your views here.
 
@@ -24,3 +24,17 @@ def userProfileView(request, username):
     discussions_user = Discussion.objects.filter(author_discussion=user).order_by("-pk")
     context = {"user": user, "discussions_user": discussions_user}
     return render(request, 'core/user_profile.html', context)
+
+def search(request):
+    if "q" in request.GET:
+        querystring = request.GET.get("q")
+        print(querystring)
+        if len(querystring) == 0:
+          return redirect("/search/")
+        discussions = Discussion.objects.filter(title__icontains=querystring)
+        posts = Post.objects.filter(content__icontains=querystring)
+        users = User.objects.filter(username__icontains=querystring)
+        context = {"discussions": discussions, "posts": posts, "users": users}
+        return render(request, 'core/search.html', context)
+    else:
+        return render(request, 'core/search.html')
